c8c7eefba1b8e199022e14eef3a96b26788af48b
dhannya <dhannya25115@gmail.com>
2024-02-02 17:36:10 +0530

    SEC commit

--- LeorElla/settings.py
+++ LeorElla/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vvm##%pfgrvfzf(3uce1&+2-*9m-j8j+bvmitd%$^aprz@1#wf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
